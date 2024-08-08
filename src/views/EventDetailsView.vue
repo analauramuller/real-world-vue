1d9369d4aa7e795ab03ca63697df7266ac06eac0
Ana Laura S. Muller <analaurasilvamuller@gmail.com>
2024-08-08 10:04:55 -0300

    Atividade Vue

---
+++ src/views/EventDetailsView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const event = ref(null)
+
+onMounted(() => {
+  // fetch event (by id) and set local event data
+  // busca o evento (por id) e define os dados do evento local
+  
+})
+</script>
+
+<template>
+  <div>
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
